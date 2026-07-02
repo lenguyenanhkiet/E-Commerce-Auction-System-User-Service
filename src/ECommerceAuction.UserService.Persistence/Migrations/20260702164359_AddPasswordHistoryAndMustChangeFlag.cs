d129248c71da05fd259a87de1d29d349f106919a
Le Nguyen Anh Kiet <kietlnase184435@fpt.edu.vn>
2026-07-03 00:31:37 +0700

    Fix: Modify the migration to use AddColumn instead of RenameColumn for must_change_password.

--- src/ECommerceAuction.UserService.Persistence/Migrations/20260702164359_AddPasswordHistoryAndMustChangeFlag.cs
+++ src/ECommerceAuction.UserService.Persistence/Migrations/20260702164359_AddPasswordHistoryAndMustChangeFlag.cs
@@ -10,41 +10,22 @@ namespace ECommerceAuction.UserService.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "MustChangePassword",
-                schema: "user",
-                table: "Users",
-                newName: "must_change_password");
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "must_change_password",
-                schema: "user",
-                table: "Users",
-                type: "bit",
-                nullable: false,
-                defaultValue: false,
-                oldClrType: typeof(bool),
-                oldType: "bit");
+               migrationBuilder.AddColumn<bool>(
+               name: "must_change_password",
+               schema: "user",
+               table: "Users",
+               type: "bit",
+               nullable: false,
+               defaultValue: false);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.RenameColumn(
-                name: "must_change_password",
-                schema: "user",
-                table: "Users",
-                newName: "MustChangePassword");
-
-            migrationBuilder.AlterColumn<bool>(
-                name: "MustChangePassword",
-                schema: "user",
-                table: "Users",
-                type: "bit",
-                nullable: false,
-                oldClrType: typeof(bool),
-                oldType: "bit",
-                oldDefaultValue: false);
+               migrationBuilder.DropColumn(
+               name: "must_change_password",
+               schema: "user",
+               table: "Users");
         }
     }
 }
